9722a35fe6f346b1e1aa11c97606cfd06f576d1b
IBRAPZ <pzmz1718@gmail.com>
2025-08-16 18:16:35 -0500

    Eliminado ComponenteTres de ComponenteDos

--- src/components/ComponenteDos.jsx
+++ src/components/ComponenteDos.jsx
@@ -1,16 +1,8 @@
-import ComponenteTres from './ComponenteTres';
 import '../styles/ComponenteDos.css';
-
 
 function ComponenteDos() {
   return (
     <div className="componente-dos">
-      <div className="fila-tres">
-        <ComponenteTres titulo="TÍTULO" />
-        <ComponenteTres titulo="TÍTULO" />
-        <ComponenteTres titulo="TÍTULO" />
-        <ComponenteTres titulo="TÍTULO" />
-      </div>
       <div className="footer-dos">COMPONENTE DOS</div>
     </div>
   );
